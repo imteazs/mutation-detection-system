a3cfc856b1e6500647d174ab85650e457db6a0b5
imteazs <imteazs@gmail.com>
2020-04-04 21:36:02 -0700

    testing from pycharm

--- src/MutDetect.py
+++ src/MutDetect.py
@@ -1,1 +1,16 @@
-test test
+import pathlib
+import argparse
+
+
+def main():
+    progparser = argparse.ArgumentParser(
+        description="This program is designed accept the directory location of fastq files"
+                    "and location of the bowtie reference index. Afterwards it will align"
+                    "them in bowtie and pass the bam file into a mutation detection "
+                    "package in GATK")
+    progparser.add_argument("--fastq", help="directory to fastq files")
+    progparser.add_argument("--reference", help="directory to reference")
+
+
+if __name__ == "__main__":
+    main()
