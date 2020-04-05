4a9b83b93ba45ebc65d6f89b24111052a3a7074f
imteazs <imteazs@gmail.com>
2020-04-04 22:34:23 -0700

    trying to get bowtie to work in python

--- src/BowtieRun.py
+++ src/BowtieRun.py
@@ -1,7 +1,10 @@
 import cmd
 
-def bowtieRun(fastq_list):
+
+def bowtieRun(fastq_list, refpath, index):
     pathstring = ''
     for item in fastq_list:
         pathstring = pathstring + ',' + str(item)
-    print(pathstring)
+    fastq_string = pathstring.lstrip(',')
+    termcmd = cmd.Cmd()
+    termcmd.precmd("bowtie2 -h")
--- src/MutDetect.py
+++ src/MutDetect.py
@@ -8,13 +8,17 @@ def main():
                     " and location of the bowtie reference index. Afterwards it will align"
                     "them in bowtie and pass the bam file into a mutation detection "
                     "package in GATK")
+
     progparser.add_argument("--fastq", help="directory to fastq files")
     progparser.add_argument("--ref", help="directory to reference")
+    progparser.add_argument("--idxname", help="Name of bowtie index")
+
     arguments = progparser.parse_args()
     fastq = Path(arguments.fastq).glob("*.fastq.gz")
     list_fastq = list(fastq)
     ref = Path(arguments.ref)
-    BowtieRun.bowtieRun(list_fastq)
+    index = arguments.idxname
+    BowtieRun.bowtieRun(list_fastq, ref, index)
 
 if __name__ == "__main__":
     main()
